6b163345323d6aeb300f53768168dba154c24530
Nico Wagner <info@ni-wa.de>
2021-05-10 02:55:07 +0200

    fixed a type issue when given an empty array

--- src/core/sum.ts
+++ src/core/sum.ts
@@ -1,7 +1,4 @@
 // function for adding two numbers. Easy!
 const add = (a: number, b: number) => a + b;
 // use reduce to sum our array
-export default (n: number[]) => {
-  if (n.length < 1) return 0;
-  return n.reduce(add);
-};
+export default (n: number[]) => n.reduce(add, 0);
