9d028d13fd30a96b8ae09642fca8080ce1adf541
Nico Wagner <info@ni-wa.de>
2022-02-27 03:03:27 +0100

    start dataHandlers in index.ts (#7)

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,18 @@ import DataHandler from './lib/dataHandler';
 
 const dataHandler = new DataHandler();
 
+//load dataHandlers
+import Database from './dataHandler/database';
+const db = new Database();
+import SocketIO from './dataHandler/socket';
+const socket = new SocketIO();
+
 process.on('SIGINT', () => {
   console.log('\n');
-  dataHandler.close().then(() => {
-    process.exit(0);
-  });
+  db.cleanup()
+    .then(dataHandler.close)
+    .then(socket.cleanup)
+    .then(() => {
+      process.exit(0);
+    });
 });
